86294c2b4eb8235e4e6615fde6012953b95d9306
skonovorotskiy <skonovorotskiy@gmail.com>
2015-08-26 19:33:22 +0300

    TUXMOBILE-10605 - CLONE - Implement working day indication

--- Example/MSDayColumnHeaderBackground.m
+++ Example/MSDayColumnHeaderBackground.m
@@ -14,7 +14,7 @@
 {
     self = [super initWithFrame:frame];
     if (self) {
-        self.backgroundColor = [UIColor colorWithHexString:@"f7f7f7"];
+        self.backgroundColor = [UIColor whiteColor];
     }
     return self;
 }
